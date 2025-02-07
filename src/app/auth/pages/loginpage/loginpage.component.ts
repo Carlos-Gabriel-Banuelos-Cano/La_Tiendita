30a5faf58109501517fe9742a2abe860f36c2357
MarcoDP <delgadomarco2007@gmail.com>
2025-02-07 08:36:34 -0600

    Implementación de validaciones

--- src/app/auth/pages/loginpage/loginpage.component.ts
+++ src/app/auth/pages/loginpage/loginpage.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-loginpage',
@@ -7,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrl: './loginpage.component.css'
 })
 export class LoginpageComponent {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(16)]]
+    });
+  }
+
+  login() {
+    if (this.loginForm.valid) {
+      console.log('Login exitoso', this.loginForm.value);
+    } else {
+      console.log('Formulario inválido');
+    }
+  }
 
 }
