e94e8bdff5c71fd083ec603ca3964d4c4b054f70
CarlosGBC <banucanocarlos@gmail.com>
2025-02-09 18:17:54 -0600

    Se agregó buscador a bebida y corrigió errores de importacion

--- src/app/abarrotes/abarrotes.page.ts
+++ src/app/abarrotes/abarrotes.page.ts
@@ -7,36 +7,39 @@ import { Router } from '@angular/router';
   styleUrls: ['./abarrotes.page.scss'],
   standalone: false,
 })
+
+
 export class AbarrotesPage implements OnInit {
-  productos: { id: number; nombre: string; descripcion: string; precio:number; imagen: string; }[] = [];
+  productos: { id: number; nombre: string; descripcion: string; precio: number; imagen: string;}[] = [];
 
-  constructor(private router: Router) {}
-  inicializar() {
-  this.productos = [
-    { id: 1, nombre: 'Arroz Schettino', descripcion: 'Arroz Schettino Super Extra 900g', precio: 16.00, imagen: 'assets/img/arrox.png' },
-    { id: 2, nombre: 'Frijol Negro', descripcion: 'Frijol Negro La Sierra 1kg', precio: 24.50, imagen: 'assets/img/frijoles.png' },
-    { id: 3, nombre: 'Aceite Nutrioli', descripcion: 'Aceite comestible 123 1L', precio: 30.00, imagen: 'assets/img/nutrioli.png' },
-    { id: 4, nombre: 'Sal La Fina', descripcion: 'Sal La Fina 1kg', precio: 25.00, imagen: 'assets/img/Sal.png'},
-    { id: 5, nombre: 'Cereal Cheerios', descripcion: 'Cereal Cheerios 340g', precio: 80.00, imagen: 'assets/img/cereal.png'},
-    { id: 6, nombre: 'Azúcar Bueno', descripcion: 'Azúcar Bueno 2Kg', precio: 49.00, imagen: 'assets/img/azucar.png'}
+  constructor(private router: Router) {
+    this.inicializar();
 
-  ];
-}
-  
-buscar(ev: any) {
-  this.inicializar();
-
-  const val = ev.target.value;
-
-  if (val && val.trim() !== '') {
-    this.productos = this.productos.filter((item) => {
-      return item.nombre.toLowerCase().includes(val.toLowerCase());
-    });
   }
-}
 
-  irACarrito() {
-    this.router.navigate(['/carrito']);
+  inicializar() {
+
+    this.productos = [
+      { id: 1, nombre: 'Arroz Schettino', descripcion: 'Arroz Schettino Super Extra 900g', precio: 16.00, imagen: 'assets/img/arrox.png' },
+      { id: 2, nombre: 'Frijol Negro', descripcion: 'Frijol Negro La Sierra 1kg', precio: 24.50, imagen: 'assets/img/frijoles.png' },
+      { id: 3, nombre: 'Aceite Nutrioli', descripcion: 'Aceite comestible 123 1L', precio: 30.00, imagen: 'assets/img/nutrioli.png' },
+      { id: 4, nombre: 'Sal La Fina', descripcion: 'Sal La Fina 1kg', precio: 25.00, imagen: 'assets/img/Sal.png'},
+      { id: 5, nombre: 'Cereal Cheerios', descripcion: 'Cereal Cheerios 340g', precio: 80.00, imagen: 'assets/img/cereal.png'},
+      { id: 6, nombre: 'Azúcar Bueno', descripcion: 'Azúcar Bueno 2Kg', precio: 49.00, imagen: 'assets/img/azucar.png'}
+  
+    ];
+  }
+
+  buscar(ev: any) {
+    this.inicializar();
+
+    const val = ev.target.value;
+
+    if (val && val.trim() !== '') {
+      this.productos = this.productos.filter((item) => {
+        return item.nombre.toLowerCase().includes(val.toLowerCase());
+      });
+    }
   }
 
   agregarAlCarrito(producto: any) {
@@ -46,5 +49,10 @@ buscar(ev: any) {
     alert('Producto añadido al carrito');
   }
 
+  irACarrito() {
+    this.router.navigate(['/carrito']);
+  }
+
   ngOnInit() {}
+
 }
--- src/app/bebidas/bebidas.page.ts
+++ src/app/bebidas/bebidas.page.ts
@@ -9,21 +9,34 @@ import { Router } from '@angular/router';
 
 })
 export class BebidasPage implements OnInit {
+  productos: { id: number; nombre: string; descripcion: string; precio: number; imagen: string;}[] = [];
 
-  productos = [
-    { id: 1, nombre: 'Boing de Mango', descripcion: 'Boing de Mango 500ml', precio: 15.00, imagen: 'assets/img/boing.png' },
-    { id: 2, nombre: 'Monster Energy', descripcion: 'Monster Energy 473ml', precio: 40.00, imagen: 'assets/img/monster.png' },
-    { id: 3, nombre: 'Refresco CocaCola', descripcion: 'Refresco CocaCola 750ml', precio: 25.00, imagen: 'assets/img/coca.png' }
+  constructor(private router: Router) {
+    this.inicializar();
+
+  }
+
+  inicializar() {
+
+    this.productos = [
+      { id: 1, nombre: 'Boing de Mango', descripcion: 'Boing de Mango 500ml', precio: 15.00, imagen: 'assets/img/boing.png' },
+      { id: 2, nombre: 'Monster Energy', descripcion: 'Monster Energy 473ml', precio: 40.00, imagen: 'assets/img/monster.png' },
+      { id: 3, nombre: 'Refresco CocaCola', descripcion: 'Refresco CocaCola 750ml', precio: 25.00, imagen: 'assets/img/coca.png' }
+    
   
+    ];
+  }
 
-  ];
+  buscar(ev: any) {
+    this.inicializar();
 
-  constructor(private router: Router) {}
+    const val = ev.target.value;
 
-  ngOnInit() {}
-
-  irACarrito() {
-    this.router.navigate(['/carrito']);
+    if (val && val.trim() !== '') {
+      this.productos = this.productos.filter((item) => {
+        return item.nombre.toLowerCase().includes(val.toLowerCase());
+      });
+    }
   }
 
   agregarAlCarrito(producto: any) {
@@ -32,4 +45,11 @@ export class BebidasPage implements OnInit {
     localStorage.setItem('carrito', JSON.stringify(carrito));
     alert('Producto añadido al carrito');
   }
+
+  irACarrito() {
+    this.router.navigate(['/carrito']);
+  }
+
+  ngOnInit() {}
+
 }
