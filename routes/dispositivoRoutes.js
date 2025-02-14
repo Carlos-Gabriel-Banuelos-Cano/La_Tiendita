bb2333d6676833b4ea22e455b789ff1205c613ed
MarcoDP <delgadomarco2007@gmail.com>
2025-02-14 09:07:49 -0600

    Actualización de las rutas de Dispositivo

--- routes/dispositivoRoutes.js
+++ routes/dispositivoRoutes.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const dispositivoController = require('../controller/dispositivoController');
 
+router.get('/get', dispositivoController.getAllDispositivos);
 router.get('/get/:id', dispositivoController.getDispositivoById);
 router.put('/update/:id', dispositivoController.updateDispositivo);
+router.post('/create', dispositivoController.createDispositivo);
 router.delete('/delete/:id', dispositivoController.deleteDispositivo);
 
 
