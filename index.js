3ad4cb1f1a768d044aa332b526fab27b1c95cfc6
MarcoDP <delgadomarco2007@gmail.com>
2025-03-05 12:46:58 -0600

    Actualización del Index

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const connectDB = require('./config/db');
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./config/swagger'); 
+const dispositivoRoutes = require('./routes/dispositivoRoutes');
+const eventoRoutes = require('./routes/eventoRoutes');
+const cors = require('cors'); 
+
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(cors()); 
 
 connectDB();
 
-const dispositivoRoutes = require('./routes/dispositivoRoutes');
-const eventoRoutes = require('./routes/eventoRoutes');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use('/dispositivos', dispositivoRoutes);
 app.use('/eventos', eventoRoutes);
 
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Algo salió mal en el servidor' });
+});
+
 app.listen(port, () => {
   console.log(`API corriendo en http://localhost:${port}`);
+  console.log(`Documentación en http://localhost:${port}/api-docs`);
 });
